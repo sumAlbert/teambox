dd136a709ce190a2cd4dfc0d32b0d758eb4b2c86
sumAlbert <982675088@qq.com>
2017-09-18 11:37:14 +0800

    plan to use webstorm develop this web

--- js/personAccountSafe.js
+++ js/personAccountSafe.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
+	var json_Info={
+		switch_name:true,
+		switch_school:true,
+		switch_tel:true,
+		switch_qq:true,
+		switch_wechat:true,
+		switch_hot:true,
+		switch_stop:true,
+		switch_save:true
+	};//记录按钮的值
 	/*锁，避免多次运行*/
 	var hover_lock=false;
+	var click_lock=false;
 	/*标题栏右边的菜单*/
 	$(".person-menu").hover(function(){
 		if(!hover_lock)
@@ -15,6 +26,32 @@ $(document).ready(function(){
 		$(".person-menu-hidden").animate({height:'0em'},200);
 		$(".person-menu-hidden").fadeOut(100);
 	});
+	/*按钮切换*/
+	$(".switch-outter").click(function () {
+		if(!click_lock){
+			click_lock=true;
+			var switch_item_name=$(this).attr('class').split(' ')[1];
+            console.log(json_Info[switch_item_name]);
+			if(json_Info[switch_item_name]){
+                json_Info[switch_item_name]=false;
+                $(this).children(".switch-inner").css({background:'#ddd'});
+                $(this).children(".switch-inner").animate({
+					right:'2em'
+                },300,function () {
+                    click_lock=false;
+                });
+			}
+			else{
+                json_Info[switch_item_name]=true;
+                $(this).children(".switch-inner").css({background:'rgba(26, 188, 156, 1)'});
+                $(this).children(".switch-inner").animate({
+					right:'0em'
+                },300,function () {
+                    click_lock=false;
+                });
+			}
+		}
+    });
 	/*表单提交*/
 	// $(".content-part-item-submit-button").click(function(){
 	// 	name=$(".input-name").val();
