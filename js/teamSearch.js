362a66d4be3fabf422fa4580af65b09293a4290a
sumAlbert <982675088@qq.com>
2017-10-22 00:05:02 +0800

    finish the personSearch.html and teamSearch.html

---
+++ js/teamSearch.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	/*提交的元素*/
+
+	/*锁，避免多次运行*/
+	var hover_lock=false;
+	/*标题栏右边的菜单*/
+	$(".person-menu").hover(function(){
+		if(!hover_lock)
+		{	
+			hover_lock=true;
+			$(".person-menu-hidden").fadeIn(50);
+			$(".person-menu-hidden").animate({height:'7em'},100,function(){
+				hover_lock=false;
+			});
+		}
+	},function(){
+		$(".person-menu-hidden").animate({height:'0em'},200);
+		$(".person-menu-hidden").fadeOut(100);
+	});
+});
